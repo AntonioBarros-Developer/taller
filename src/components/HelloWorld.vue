10284d9f721983b1cd2ab0f781c67d0542261a20
Antonio jose Barros villar <antoniojosebarrosvillar2@gmail.com>
2024-09-18 18:02:55 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <header class="encabezado">
+      <h1>POKEMON</h1>
+    </header>
+
+
+
+    <main class="contenido">
+
+      <div v-for="CajaPokemon in Pokemon" v-bind:key="CajaPokemon" class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img  :src="CajaPokemon.imagen" :alt="CajaPokemon.id">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+      </div>
+      <div class="media-content">
+        <p class="title is-4">{{ CajaPokemon.nombre }}</p>
+        <div class="borderColor" :style="{backgroundColor: CajaPokemon.type_color }">
+        <h3>{{ CajaPokemon.type_name }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </main>
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data: function(){
+    return{
+      Pokemon:[]
+    }
+  },
+  methods:{
+    Pokemones(){
+      axios
+      .get("https://cobuses.com.co/APIV2/model/pokemon.php?dato=getallpokemon")
+      .then((Respuesta)=>{
+        this.Pokemon = Respuesta.data
+        console.log(Respuesta)
+      })
+      .catch((errorapi)=>{
+        alert("LO SENTIMOS API ESTA EN MANTENIMIENTO"+errorapi)
+      })
+    }
+  },
+mounted(){
+  this.Pokemones();
 }
+  
+}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.encabezado{
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   background-color: black;
+   height: 80px;
+   color: yellow;
+   font-size: 40px;
+   margin-bottom: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.contenido{
+  display: grid;
+  grid-template-columns: repeat(4,1fr); 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.card{
+width: 300px;
+border-radius: 10px;
 }
-a {
-  color: #42b983;
+.borderColor{
+  color: white;
+  font-size: 20px;
+  border-radius: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import primevue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import bulma from 'bulma/css/bulma.css'
+ const app = createApp(App).use(router)
+app.use(VueAxios,axios)
+app.use(bulma)
+app.use(primevue,{
+    Themes:{
+        presets:Aura
+    }
+})
 
-createApp(App).mount('#app')
+.mount('#app')
+export{app}
